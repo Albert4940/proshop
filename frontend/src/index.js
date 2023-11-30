58af1d98a24ec95f4c9e0f03bd53732adfc8e101
Albert4940 <dorcealbertmary@gmail.com>
2023-11-29 20:46:32 -0500

    Add custom Bootstrap Theme And List Products

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './index.css';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider
+} from 'react-router-dom';
+// import 'bootstrap/dist/css/bootstrap.min.css'
+import './assets/styles/bootstrap.custom.css';
+import './assets/styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
