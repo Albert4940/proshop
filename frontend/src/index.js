8988dc67f6e5af62fca0e9208a503144246e5590
Albert4940 <dorcealbertmary@gmail.com>
2023-11-30 21:07:02 -0500

    Implement react-router and react-router-bootstrap

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,12 +10,20 @@ import {
 import './assets/styles/bootstrap.custom.css';
 import './assets/styles/index.css';
 import App from './App';
+import HomeScreen from './screens/HomeScreen';
 import reportWebVitals from './reportWebVitals';
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<HomeScreen/>}/>
+    </Route>
+  )
+)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
