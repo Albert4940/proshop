861a98b05f638c78878e82daa59775b45cb311ce
Albert4940 <dorcealbertmary@gmail.com>
2024-01-01 21:32:26 -0500

    feat:PS-48702230-Logout User & Clear Cookie

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -38,7 +38,12 @@ export const registerUser = asyncHandler(async (req, res) => {
 //@route POST /api/users/auth
 //@access Public
 export const logoutUser = asyncHandler(async (req, res) => {
-    res.send("Logout User");
+    res.cookie('jwt','',{
+        httpOnly:true,
+        expires: new Date(0)
+    });
+
+    res.status(200).json({message: 'Logged out successfully'});
 })
 
 //@desc Auth user & get token
