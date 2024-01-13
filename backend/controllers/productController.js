3910df0a9bb16d49a63cac394c75646b5bdcf47f
Albert4940 <dorcealbertmary@gmail.com>
2024-01-12 23:48:12 -0500

    feat:PS-49854732-Add Paginate Products

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -5,14 +5,23 @@ import Product from "../models/productModel.js";
 //@route GET /api/products
 //@access Public
 const getProducts = asyncHandler(async (req, res) => {
-  const products = await Product.find({});
-  res.json(products);
+  const pageSize = process.env.PAGINATION_LIMIT;
+  const page = Number(req.query.pageNumber) || 1;
+
+  const count = await Product.countDocuments();
+  
+  const products = await Product.find({})
+    .limit(pageSize)
+    .skip(pageSize * (page - 1));
+
+  res.json({products, page, pages: Math.ceil(count / pageSize)});
 });
 
 //@desc Fetch a product
 //@route GET /api/products/:id
 //@access Public
 const getProductById = asyncHandler(async (req,res) => {
+
   const product = await Product.findById(req.params.id);
     if(product){
         return res.json(product);
