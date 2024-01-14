5d517e5aee58f7a6e1b35c32128b0b9b49d082e2
Albert4940 <dorcealbertmary@gmail.com>
2024-01-14 16:05:37 -0500

    feat:PS-49854732-Add Search products feature

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -4,9 +4,11 @@ import { useGetProductsQuery } from '../slices/productsApiSlice'
 import Loader from '../components/Loader'
 import Message from '../components/Message'
 import { useParams } from 'react-router-dom'
+import Paginate from '../components/Paginate'
+
 const HomeScreen = () => {
-    const {pageNumber} = useParams();
-    const {data, isLoading, error} = useGetProductsQuery({pageNumber});    
+    const {pageNumber, keyword} = useParams();
+    const {data, isLoading, error} = useGetProductsQuery({keyword,pageNumber});    
 
     return (
         <>
@@ -26,9 +28,14 @@ const HomeScreen = () => {
                             </Col>
                         ))}
                     </Row>
+                    <Paginate
+                        pages={data.pages}
+                        page={data.page}
+                        keyword={keyword ? keyword : ''}
+                    />
                 </>
             )}
-        </>
+        </> 
     )
 }
 
