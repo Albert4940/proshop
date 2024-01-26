cc72c735a7dcc975ad374f2907edf51a4d2c733a
Albert4940 <dorcealbertmary@gmail.com>
2024-01-26 05:01:28 -0500

    no-feat: Set up for production

--- backend/server.js
+++ backend/server.js
@@ -23,9 +23,6 @@ app.use(cookieParser())
 
 app.use(cors());
 
-app.get('/',(req, res) => {
-    res.send('API is running...');
-});
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
@@ -34,8 +31,21 @@ app.use('/api/upload', uploadRoutes)
 app.get('/api/config/paypal', (req, res) => res.send({clientId: 
     process.env.PAYPAL_CLIENT_ID}));
     
-const __dirname = path.resolve();
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+if(process.env.NODE_ENV === "production"){
+    const __dirname = path.resolve();
+    app.use('/uploads', express.static('/var/data/uploads'))
+    app.use(express.static(path.join(__dirname,'/frontend/build')));
+
+    app.get('*',(req,res) => res.sendFile(path.resolve(__dirname, 'frontend','build','index.html')));
+}else{
+    const __dirname = path.resolve();
+    app.use('/uploads', express.static(path.join(__dirname,'/uploads')));
+
+    app.get('/', (req, res) => res.send('API is running....'))
+}
+
+/*const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))*/
 
 app.use(notFound);
 app.use(errorHandler);
